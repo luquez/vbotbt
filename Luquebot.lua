2ad6a03dd96510deb95e760ce37ba275fcaf7cc2
luquez <54032598+luquez@users.noreply.github.com>
2025-10-17 12:15:33 +0100

    Update Luquebot.lua

--- Luquebot.lua
+++ Luquebot.lua
@@ -3,15 +3,6 @@
 
 setDefaultTab("Luque")
 
-macro(200, "Auto Haste", function()
-  if isInPz() then return end          -- não usa em PZ
-  if hasHaste() then return end        -- já tem haste ativo
-  if getSpellCoolDown("utani hur") then return end  -- ainda em cooldown
-  if mana() < 40 then return end       -- sem mana suficiente
-
-  say("Haste")                     -- lança a spell
-end)
-
 lblInfo= UI.Label("gathering")
 lblInfo:setColor("red")
 
