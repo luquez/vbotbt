0e29139f0227500d1ec377ea0e5b2fe43ea08d5b
luquez <54032598+luquez@users.noreply.github.com>
2026-06-09 05:03:26 +0100

    Update harmonic.lua

--- harmonic.lua
+++ harmonic.lua
@@ -1,11 +1,10 @@
 setDefaultTab("Harmonic")
 
-lblInfo= UI.Label("Harmonic")
+lblInfo = UI.Label("Harmonic")
 lblInfo:setColor("red")
 
 UI.Separator()
 
--- classe_void.lua
 local version = "1.0"
 print("[Luquebot] Classe: Harmonic carregada v" .. version)
 
@@ -13,16 +12,15 @@ UI.Separator()
 
 macro(200, "Combo - Harmonic", function()
   if g_game.isAttacking() then
-  say("Accelerando")       -- Level 1600
-  say("Triad")
-  say("Double Chord")
-  say("Resonance")         -- Level 800
-  say("Divine Ballad")     -- Level 300
-  say("Crescendo")         -- Level 120
-  say("Magic String")      -- Level 65
-  say("Reverberate")       -- Level 25
-  say("Music Strike")      -- Level 8
-
+    say("Accelerando")       -- Level 1600
+    say("Triad")
+    say("Double Chord")
+    say("Resonance")         -- Level 800
+    say("Divine Ballad")     -- Level 300
+    say("Crescendo")         -- Level 120
+    say("Magic String")      -- Level 65
+    say("Reverberate")       -- Level 25
+    say("Music Strike")      -- Level 8
   end
 end)
 
@@ -31,105 +29,88 @@ UI.Separator()
 macro(200, "Combo - ++", function()
   if g_game.isAttacking() then
     say("Triad")
-    say("Accelerando")
+    say("Accelerando")       -- Level 1600
     say("Double Chord")
-    say("Resonance")
-    say("Divine Ballad")
-    say("Crescendo")
-    say("Magic String")
+    say("Resonance")         -- Level 800
+    say("Divine Ballad")     -- Level 300
+    say("Crescendo")         -- Level 120
+    say("Magic String")      -- Level 65
   end
 end)
 
 UI.Separator()
 
--- Buff 1: Void
---macro(7000, "Voidwalking", function()
---  if not isInPz() then
---    say("Voidwalk") -- magia do buff
---  end
---end)  -- 🔹 fecha macro Buff Void
-
-
--- Buff 1: Void
-macro(6000, "Buff Bardo", function()
+macro(8000, "Buff Bardo", function()
   if not isInPz() then
-    say("Echo Song") -- magia do buff
+    say("Echo Song")
   end
-end)  -- 🔹 fecha macro Buff Void
+end)
 
 UI.Separator()
 
 local spell = "Mana Waste"
-local manaMin = 20       -- mínimo de mana% para castar
-local interval = 3000    -- 3 segundos
+local manaMin = 20
+local interval = 3000
 
 macro(interval, "Mana Training", function()
---    if isInPz() then return end           -- bloqueia dentro de PZ
-    if manapercent() < manaMin then return end
-    say(spell)
+  if manapercent() < manaMin then return end
+  say(spell)
 end)
 
 UI.Separator()
 
-lblInfo = UI.Label("")
 lblInfo = UI.Label("Heal")
 lblInfo:setColor("blue")
 
 UI.Separator()
 
+local healingSpell1 = "Intense Wound Cleansing"
+local healthPercent1 = 97
 
-local healingSpell = "Intense Wound Cleansing"
-local healthPercent = 97
 macro(200, "Intense Wound Cleansing", function()
-  if isInPz() then return end   
-  if hppercent() <= healthPercent then
-    say(healingSpell)
+  if isInPz() then return end
+  if hppercent() <= healthPercent1 then
+    say(healingSpell1)
   end
 end)
 
 UI.Separator()
 
+local healingSpell2 = "Limb Restoration"
+local healthPercent2 = 97
 
-local healingSpell = "Limb Restoration"
-local healthPercent = 97
 macro(200, "Limb Restoration", function()
-  if isInPz() then return end   
-  if hppercent() <= healthPercent then
-    say(healingSpell)
+  if isInPz() then return end
+  if hppercent() <= healthPercent2 then
+    say(healingSpell2)
   end
 end)
 
 UI.Separator()
 
-
-lblInfo = UI.Label("")
 lblInfo = UI.Label("Potion HP")
 lblInfo:setColor("blue")
 addSeparator()
 Panels.HealthItem()
-UI.Separator()
 
 UI.Separator()
 
-lblInfo = UI.Label("")
 lblInfo = UI.Label("MANA para noobs")
 lblInfo:setColor("blue")
 addSeparator()
 addSeparator()
 Panels.ManaItem()
+
 UI.Separator()
 
+local potId = 7643
+local potInterval = 500
 
-local potId = 7643  -- ID da potion
-local interval = 500  -- 0.5s
-
-macro(interval, "Pot Craft SPAM", function()
-    usewith(potId, player)
+macro(potInterval, "Pot Craft SPAM", function()
+  usewith(potId, player)
 end)
 
 UI.Separator()
-
-
 
 macro(10000, "FOOD", function()
   if player:getRegenerationTime() > 400 then return end
@@ -143,6 +124,7 @@ macro(10000, "FOOD", function()
   end
 end)
 
+UI.Separator()
 
 macro(10000, "FOOD - Passe", function()
   if player:getRegenerationTime() > 400 then return end
@@ -156,27 +138,34 @@ macro(10000, "FOOD - Passe", function()
   end
 end)
 
-macro(200, "Auto Haste", function()
-  if isInPz() then return end          -- não usa em PZ
-  if hasHaste() then return end        -- já tem haste ativo
-  if getSpellCoolDown("utani hur") then return end  -- ainda em cooldown
-  if mana() < 40 then return end       -- sem mana suficiente
+UI.Separator()
 
-  say("Haste")                     -- lança a spell
+macro(200, "Auto Haste", function()
+  if isInPz() then return end
+  if hasHaste() then return end
+  if getSpellCoolDown("utani hur") then return end
+  if mana() < 40 then return end
+
+  say("Haste")
 end)
 
 UI.Separator()
--- upda / down
-lblInfo= UI.Label("Para up / Down")
-lblInfo= UI.Label("Space")
+
+lblInfo = UI.Label("Para up / Down")
 lblInfo:setColor("green")
+
+lblInfo = UI.Label("Space")
+lblInfo:setColor("green")
+
 UI.Separator()
+
 onKeyPress(function(keys)
   if keys == "Space" then
-    say('levitate "up') 
-    say('levitate "down') 
+    say('levitate "up')
+    say('levitate "down')
   end
 end)
+
 UI.Separator()
 
 hotkey("F12", function()
